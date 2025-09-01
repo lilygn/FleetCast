0e7ca9a25cc65228e5f8520295056db598fb5325
lilygniedz <lilygn6@gmail.com>
2025-08-31 19:26:42 -0500

    Revert repository to state of 7c55a63

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Dashboard.css';
-import api from './api'
 
 function Dashboard() {
   const [summary, setSummary] = useState(null);
@@ -10,7 +9,7 @@ function Dashboard() {
   // Fetch summary using Axios
   useEffect(() => {
     const fetchData = () => {
-      api.get('/api/dashboard')
+        axios.get(`'http://localhost:3000/api/dashboard`)
   .then(res => {
     console.log("Dashboard data:", res.data);  // <-- Add this
     setSummary(res.data);
